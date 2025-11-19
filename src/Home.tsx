f110e62df9ffcac297490577cd43f552d6f6ac10
KPSecretAccount <sampleemail4github@gmail.com>
2025-11-19 13:39:29 -0500

    :)

--- src/Home.tsx
+++ src/Home.tsx
@@ -12,14 +12,6 @@ export default function Home() {
                     blueprint. The result? Higher costs, unhealthy living environments and a harmful impact on our
                     planet. Heritage Home Co. fills this gap by designing the home YOU want – prioritizing the planet
                     and people, ultimately offering comfort, long term sustainability and peace of mind.</p>
-               <h1><br></br>About</h1>
-                <img src="found.png" alt="founder picture"/>
-                <h2>Michaela Aiello</h2>
-                <h3>CEO and Founder</h3>
-                <p>Growing up the youngest of five in a lower middle class family, everything was a DIY. My dads brains
-                    and the helping hands of his kids. I didn't see helping as something to dread but rather found
-                    enjoyment in it. I've helped my dad on major projects since I was 8. I've never defined myself as a
-                    "girly girl" but rather a girl- now woman- who isn't afraid of following her dreams.</p>
             </div>
         </div>
     );
